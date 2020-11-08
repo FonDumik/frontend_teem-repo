6f25d2fc72c5916c4e997345ecc951fe43700220
FonDumik <fondumik@yandex.ru>
2020-11-08 18:39:40 +0500

    add js

--- instagram/js/scripts.js
+++ instagram/js/scripts.js
@@ -1,8 +1,18 @@
 function openView() {
     document.getElementById('modal_container').style = 'display: flex';
     document.getElementById('body').style = 'overflow-y: hidden';
+
+    $('.gallery_img').click(function() {
+        var imgAddr = $(this).attr("src");
+        $('#gallery_item_view').attr({src: imgAddr});
+      });
 }
+
 function closeModal() {
     document.getElementById('modal_container').style = 'display: none';
     document.getElementById('body').style = 'overflow-y: auto';
 }
+
+
+
+
