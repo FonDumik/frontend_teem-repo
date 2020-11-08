d6926fa1e3eec2f2096a5e84807b32abff2ba6fa
FonDumik <fondumik@yandex.ru>
2020-11-08 20:52:48 +0500

    change js, css

--- instagram/js/scripts.js
+++ instagram/js/scripts.js
@@ -1,18 +1,25 @@
-function openView() {
+$('.gallery_img').click(function() {
+    var imgAddr = $(this).attr("src");
+    $('#gallery_item_view').attr({src: imgAddr});
     document.getElementById('modal_container').style = 'display: flex';
     document.getElementById('body').style = 'overflow-y: hidden';
+  });
 
-    $('.gallery_img').click(function() {
-        var imgAddr = $(this).attr("src");
-        $('#gallery_item_view').attr({src: imgAddr});
-      });
-}
-
-function closeModal() {
+$('#modal_container').click(function() {
     document.getElementById('modal_container').style = 'display: none';
     document.getElementById('body').style = 'overflow-y: auto';
-}
+  });
 
+$(".follow").click(function() {
+    $(this).text(function(i, text) {
+      return text === "Follow" ? "Followed" : "Follow";
+    });
+});
+//     if ($(this).style == 'background: #ffffff; color: #4094EF; border: 1px solid #4094EF;') {
+//         $(this).style = 'background: #4094EF; color: #ffffff; border: none;';
+//     } else {
+//         $(this).style = 'background: #ffffff; color: #4094EF; border: 1px solid #4094EF;';
+//     }
 
-
+//   
 
