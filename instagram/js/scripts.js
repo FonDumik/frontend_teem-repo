c30a8ddfb057808ce4dece897285efad4a511c31
FonDumik <fondumik@yandex.ru>
2020-11-16 20:05:35 +0500

    changes

--- instagram/js/scripts.js
+++ instagram/js/scripts.js
@@ -1,25 +1,23 @@
-$('.gallery_img').click(function() {
-    var imgAddr = $(this).attr("src");
+$(document).ready(function() {
+  let container = document.getElementById('modal_container');
+  let body = document.getElementById('body');
+
+  $('.gallery_img').click(function() {
+    let imgAddr = $(this).attr("src");
     $('#gallery_item_view').attr({src: imgAddr});
-    document.getElementById('modal_container').style = 'display: flex';
-    document.getElementById('body').style = 'overflow-y: hidden';
+    container.style = 'display: flex';
+    body.style = 'overflow-y: hidden';
   });
 
-$('#modal_container').click(function() {
-    document.getElementById('modal_container').style = 'display: none';
-    document.getElementById('body').style = 'overflow-y: auto';
-  });
-
-$(".follow").click(function() {
-    $(this).text(function(i, text) {
-      return text === "Follow" ? "Followed" : "Follow";
+  $('#modal_container').click(function() {
+      container.style = 'display: none';
+      body.style = 'overflow-y: auto';
     });
+
+  $(".follow").click(function() {
+      $(this).text(function(span, text) {
+        return text == "Follow" ? "Followed" : "Follow";
+      });
+  });
+
 });
-//     if ($(this).style == 'background: #ffffff; color: #4094EF; border: 1px solid #4094EF;') {
-//         $(this).style = 'background: #4094EF; color: #ffffff; border: none;';
-//     } else {
-//         $(this).style = 'background: #ffffff; color: #4094EF; border: 1px solid #4094EF;';
-//     }
-
-//   
-
